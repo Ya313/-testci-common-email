70838e262a369553b9df4602225105c1715a73cf
Ya313 <119713571+Ya313@users.noreply.github.com>
2024-04-05 18:09:55 -0400

    Update EmailTest.java

--- src/test/java/org/apache/commons/mail/EmailTest.java
+++ src/test/java/org/apache/commons/mail/EmailTest.java
@@ -251,12 +251,6 @@ public class EmailTest {
         assertNotNull(session); // Verify that the session is not null
         assertEquals("smtp.example.com", session.getProperty("mail.smtp.host"));
     }
-
-    @Test(expected = EmailException.class)
-    //Insures that GetMailSession throws an exception when there isn't a host name 
-    public void testGetMailSessionWithoutHostname() throws EmailException {
-        email.getMailSession();
-    }
     
     @Test
     public void testGetMailSessionSSL_CheckServerIdentity() throws EmailException {
